2aa10ef674e3545ab5503fc01ad6c38e3e1ee2b4
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-14 22:34:40 +0100

    feat: create and output shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,7 +7,10 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  public ingredients: Ingredient[] = [];
+  public ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
   
   constructor() { }
 

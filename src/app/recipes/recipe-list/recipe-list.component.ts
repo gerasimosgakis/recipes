20cb5dc7ca84f265ddd703a45bf64eeb8cdf90cb
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-14 22:11:19 +0100

    feat: output recipes using ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,9 +7,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
+  public recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 
-    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
   ];
 
   constructor() { }

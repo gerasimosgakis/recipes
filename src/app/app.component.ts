d0b2284a25c74e0115e0c83aa7803f05ea91c3f9
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-15 19:18:24 +0100

    feat: add navigation using event-binding(ngIf)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipes';
+  public loadedFeature = 'recipe';
+
+  public onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }

6ea1d29d7c1f5f4537ee2fb4bd7cd86516d16ae6
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-17 18:29:27 +0100

    feat: move recipes array from recipe-list to the recipe service and using it through the service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,14 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService {
-    
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test', 
+        'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
+        new Recipe('Another Test Recipe', 'This is simply a test', 
+        'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
+    ];
+
+    public getRecipes() {
+        return this.recipes.slice();
+    }
 }

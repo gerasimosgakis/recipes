c6b237901156e25cf36ba31385791fc633e48b82
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-17 18:38:59 +0100

    feat: use an emitter in the recipeService for the selected recipe so it can't be accessd from all components avoiding the chain of eventEmitters between components

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import { Recipe } from "./recipe.model";
+import { EventEmitter } from "@angular/core";
 
 export class RecipeService {
+    public recipeSelected = new EventEmitter<Recipe>();
+    
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test', 
         'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),

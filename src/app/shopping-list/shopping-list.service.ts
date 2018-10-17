69f6c913db02d61efa356091bc59e9ccc4efeceb
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-17 18:52:19 +0100

    feat: move ingredients array to shopping list service and use it in our components

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,16 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService {
-    
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+    ];
+
+    public getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    public addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+    }
 }

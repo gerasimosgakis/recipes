34a11f3bcd26fe02d3d5875d174d49d113dcec0b
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-17 18:59:20 +0100

    feat: use services for push notifications

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,8 @@
 import { Ingredient } from "../shared/ingredient.model";
+import { EventEmitter } from "@angular/core";
 
 export class ShoppingListService {
+    public ingredientsChanged = new EventEmitter<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10)
@@ -12,5 +14,6 @@ export class ShoppingListService {
 
     public addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }

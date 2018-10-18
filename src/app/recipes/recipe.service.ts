bba69a9811b5ce8ffb1eaa295a41015685f9d005
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-18 18:37:34 +0100

    feat: configure route parameters

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -37,6 +37,10 @@ export class RecipeService {
         return this.recipes.slice();
     }
 
+    public getRecipe(index: number) {
+        return this.recipes[index];
+    }
+
     public addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngredients(ingredients);
     }
